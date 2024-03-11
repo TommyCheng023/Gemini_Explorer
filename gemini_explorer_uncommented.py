21c34fd486e53c6fca4afe80e9f66085efe88218
Tommy Cheng <xycheng@bu.edu>
2024-03-11 18:35:24 -0400

    update to the latest version

--- gemini_explorer_uncommented.py
+++ gemini_explorer_uncommented.py
@@ -4,6 +4,8 @@ import vertexai
 import streamlit as st
 from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerativeModel, Part, Content, ChatSession
+import os
+os.environ['GRPC_DNS_RESOLVER'] = 'native'
 
 project = "gemini-explorer-415722"
 vertexai.init( project = project )
